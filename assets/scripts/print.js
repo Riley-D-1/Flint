e2a4e15314b95586baa5b5dce77480cbba0bf897
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2025-10-17 22:51:27 +1100

    Added games page and naviagation
    Began styling for the signal game and tried to work on the print functionality

--- assets/scripts/print.js
+++ assets/scripts/print.js
@@ -5,8 +5,22 @@ function print_flashcards(){
     flashcards = JSON.parse(temp_flashcards)
     if (document.getElementById("print_type").value ==="Double-Sided"){
         console.log("double sided");
+        // more difficult 
+        // so the plan is for each item to match up page wise yk. 
+        // so when u print double sided it auto works 
+
+        var doc = new jsPDF();
+        doc.setFontSize(12);
+        let yPos = 10
+        flashcards.forEach((item) => {
+            console.log(item)
+            doc.text(20, yPos, item);
+            yPos += 10;
+        });
+        doc.save("myarray.pdf");
     }else{
         console.log("standard");
+        // standard will just print 2 columns
         var doc = new jsPDF();
         doc.setFontSize(12);
         let yPos = 10
