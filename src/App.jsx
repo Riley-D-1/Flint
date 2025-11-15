cfd9aa710e850ae4e91f84f2fc9653dc62581c8c
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2025-11-15 12:14:57 +1100

    React testing
    :D

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Flint Flashcards</h1>
+      <p>Welcome to my React app!</p>
+    </div>
+  );
+}
+
+export default App;
--- src/scripts/theme_selection.js
+++ src/scripts/theme_selection.js
@@ -1,3 +1,9 @@
 function check_theme(){
-    if document.getElementById("")
+    if (document.getElementById("").value = "space"){
+        // Space theme saved to local storage
+    }else if(document.getElementById("").value = "Light_theme"){
+        // light theme saved to local storage
+    }else{
+        // No theme set yet or selected dark, set dark theme to local storage
+    }
 }
