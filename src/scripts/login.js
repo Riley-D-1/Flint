bc8cc37bf6130835dc9236a1f64dd5e03cca053c
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2025-11-16 22:16:16 +1100

    Update login.js

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,8 +1,8 @@
 // I know that this is not safe but I dont care tbh, there is a big disclosure on the signup page and nobobdy is actually using this app atm lol. 
 // + the alternatives are really complex and not sure if I want to do them. 
 function login_(){
-    username_ = document.getElementById("username_")
-    let password_ = document.getElementById("password")
+    username_ = document.getElementById("username_").value
+    let password_ = document.getElementById("password").value
     if (username_ === localStorage.getItem("username") && password_ === localStorage.getItem("password")) {
         alert("Login successful!");
         localStorage.setItem("logged_in",true)
