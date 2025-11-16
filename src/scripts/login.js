491d49ff84ad5e1c93b89a0fa581eab607bbf3d1
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2025-11-16 22:13:28 +1100

    Done fr

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,15 +1,17 @@
 // I know that this is not safe but I dont care tbh, there is a big disclosure on the signup page and nobobdy is actually using this app atm lol. 
 // + the alternatives are really complex and not sure if I want to do them. 
 function login_(){
-if (username_ === localStorage.getItem("username") && password_ === localStorage.getItem("password")) {
-    alert("Login successful!");
-    localStorage.setItem("logged_in",true)
-} else if(!localStorage.getItem("username")  || !localStorage.getItem("password")){
-    localStorage.setItem("logged_in",true)
-    alert("Local Storage password or username missing, please make an account (remake if you already have)");
-}else
-    alert("Invalid credentials");
-}
+    username_ = document.getElementById("username_")
+    let password_ = document.getElementById("password")
+    if (username_ === localStorage.getItem("username") && password_ === localStorage.getItem("password")) {
+        alert("Login successful!");
+        localStorage.setItem("logged_in",true)
+    } else if(!localStorage.getItem("username")  || !localStorage.getItem("password")){
+        localStorage.setItem("logged_in",true)
+        alert("Local Storage password or username missing, please make an account (remake if you already have)");
+    }else
+        alert("Invalid credentials");
+    }
 function show_password_toggle(){
     let password_ = document.getElementById("password")
     if (password_.type === "password"){
