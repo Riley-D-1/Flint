0c5021fe54b7a85fc004db247edb860ba2ec622b
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2025-12-04 22:03:25 +1100

    Removing duplicates and work on match game
    Fixing the match game, needs continued work but we are getting there.

--- src/js/match_game.js
+++ src/js/match_game.js
@@ -4,7 +4,6 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 const temp_flashcards = localStorage.getItem("Flashcard");
 flashcards = JSON.parse(temp_flashcards)
-console.log(flashcards)
 // Wait hold up let me think chat 
 // So a grid of 3x4
 //canvas.addEventListener('click', function(event) {
@@ -15,25 +14,52 @@ console.log(flashcards)
         // Do nothing
     //}
 //})
+//setInterval(onTimerTick, 33)
+// Use temp values of 0 
 
+cards_array = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+]
 
-function match_cards(){
-    // Split cards into one big array that is taken from
-    flashcards.forEach(card => {
-        console.log(card)
-    });
-
-
+function shuffle_cards(array){
+    let i = array.length
+    while(i > 0){
+        let random_i = Math.floor(Math.random() * i)
+        i--
+        [array[i], array[random_i] = [
+            array[random_i],array[i]
+        ]]
+    }
 }
-function update_cards(){
-    // Stub
-    // Set the cards left 
-    let last_cards = len(remaining_cards)-12 
 
-    remaining_cards.splice(last_cards,12)
-    // 
-    if (len(cards) < 12 ){
+
+function make_match_cards(){
+    // Split cards into one big array that is taken from
+    remaining_cards = []
+	// Push all cards to new list so its easier to display
+    flashcards.forEach(card => {
+        remaining_cards.push(card.front)
+        remaining_cards.push(card.back)
+    });
+	// Shuffle cards using function
+	return remaining_cards
+}
+
+function get_slice(){
+    let list_chunk = remaining_cards.slice(-12)
+    shuffle_cards(list_chunk)
+    console.log(list_chunk)
+}
+
+function update_cards(remaining_cards){
+    if (remaining_cards.length < 12 ){
         return "No Cards Left"
+    }else{
+        let remaining_cards  = remaining_cards.splice(remaining_cards.length-6,12)
+        console.log(list_chunk)
+        return remaining_cards
     }
 }
 
@@ -41,7 +67,18 @@ function draw_bg(){
     ctx.fillStyle = 'rgb(0, 0, 60)';
     ctx.fillRect(0,0,canvas.width,canvas.height)
 }
-function draw_cards(){
+
+function get_card_info(row_num,column_num,slice){
+    if (cards_array[row_num][column_num] = 0){
+        // Fetch needed text 
+		let slice_index = row_num+column_num
+        return slice[slice_index]
+    }else{
+        return ""  
+    }
+}
+
+function draw_cards(slice){
     ctx.lineWidth  = 5
     ctx.lineWidth = 2
     ctx.strokeStyle = '#c5c4c4ff'
@@ -49,29 +86,63 @@ function draw_cards(){
     ctx.font = "20px Comic Sans"
     for(let j = 0;j<3;j++){
         // So rows is J, 
-        console.log(j)
         for(let i = 0; i<4 ;i++){
-            console.log(i)
             // so columns are i
             // X is width, y is height  
+            // The rectangles
             ctx.strokeRect((canvas.width/4)*i+50,(canvas.height/4)*j+150,(canvas.width/6),(canvas.height/5))
+            // The text elements inside are done right after so I call the required functions to fetch current cards here
+            text_content = get_card_info(j,i,slice)
+            ctx.textAlign = "center"; 
+            ctx.fillText(text_content,(canvas.width/4)*i+50 ,(canvas.height/4)*j+150 );
         }
     }
 
 }
-function next_round(){
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    update
-}
 
-function main(){
-    draw_bg()
-    draw_cards()
-    info_check = update_cards()
-    if info_check = "No Cards Left"{
-        // Stub
-    }else{
-        next_round()
+
+function check_match(card1,card2,flash_cards){
+    for(let i; i < flash_cards.length; i++){
+        let card = cards[i]
+        // Check against the flashcard thing
+        if (card.front == card1 && card.back == card2){
+            return true
+        }
+        // Check other way
+        else if (card.front == card1 && card.back == card2){
+            return true
+        }else{
+            return false
+        }
     }
 }
+function end_game(){
+    ctx.fillStyle = 'rgb(0, 0, 60)';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+}
+function match(){
+    if (check_match(card1) = true){
+       // Make the required elements turn visible 
+    }
+}
+
+function main(remaining_cards){
+    draw_bg()
+    get_slice()
+    remaining_cards_ = update_cards(remaining_cards_)
+    if(remaining_cards_ = "No Cards Left"){
+        console.log("No Cards Left")
+        // Display 
+        end_game()
+        window.location.href = 'index.html';
+        clearInterval(game_loop);
+        // Make new cards and restart the round
+    }else{
+        remaining_cards = update_cards
+    }
+    draw_cards()
+}
+remaining_cards = make_match_cards(flashcards)
+let game_loop = setInterval(main(remaining_cards), 33);
 
