2e55f6c05c51fd61baa4efceddb79bb02b951b3b
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2025-12-06 21:54:00 +1100

    PLEASE

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-//import { toNodeHandler, fromNodeHeaders } from "better-auth/node";
-//import { auth } from "./src/lib/auth.js";
-
-//app.use("/auth", auth.express());
-// Start the server
-// App pathing 
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
-app.use(express.static('src'));
-app.post('/login', () => {
-	// to be fixed
-	res.sendFile(path.join(process.cwd(), "src/sign_in.html"))
-})
-app.post('/logout', () => {
-// to be fixed
-})
-app.get("/", () => {
-	res.sendFile(path.join(process.cwd(), "src/index.html"))
-})
-app.get("/games", () => {
-	res.sendFile(path.join(process.cwd(), "src/about.html"))
-})
-app.get("/about", () => {
-	res.sendFile(path.join(process.cwd(), "src/about.html"))
-})
-app.get("/account", () => {
-	res.sendFile(path.join(process.cwd(), "src/account.html"))
-})
-// Exporting app to be used on server.js
+app.use(express.static("src"));
+app.post("/login", (req, res) => {
+  res.send("Login route not yet implemented");
+});
+app.post("/logout", (req, res) => {
+  res.send("Logout route not yet implemented");
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+app.get("/games", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "about.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "about.html"));
+});
+app.get("/account", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "account.html"));
+});
+// Exporting app
 export default app;
