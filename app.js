ed9f070aa9348c7ae8e5bfb28707511ca622db04
Riley-D-1 <171879480+Riley-D-1@users.noreply.github.com>
2025-12-06 22:01:03 +1100

    plssssss

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.post("/login", (req, res) => {
   res.send("Login route not yet implemented");
 });
